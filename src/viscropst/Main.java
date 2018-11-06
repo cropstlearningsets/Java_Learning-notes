075f1cfe4de170ecb1bb7cb446bb2b70cc7abc36
viscropst <bcct2010@live.cn>
2018-11-06 14:18:56 +0800

    Inspect Finished,Number start;
    Signed-off-by: viscropst <bcct2010@live.cn>

---
+++ src/viscropst/Main.java
@@ -0,0 +1,11 @@
+package viscropst;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String[] line = scanner.nextLine().split(" ");
+
+    }
+}
---
+++ src/viscropst/PinTiA/theInspect.java
@@ -0,0 +1,23 @@
+package viscropst.PinTiA;
+
+public class theInspect {
+    public static void main(String[] args) {
+        // write your code here
+        int N;
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String[] line = scanner.nextLine().split(" ");
+        N = Integer.parseInt(line[0]);
+        int U = Integer.parseInt(line[1]);
+        int D = Integer.parseInt(line[2]);
+        int sum = 0;
+        int i;
+        for (i = 1; sum <= N; i++) {
+            sum += U - D;
+            if (sum >= N - 1)
+                break;
+            else
+                i++;
+        }
+        System.out.println(i);
+    }
+}
